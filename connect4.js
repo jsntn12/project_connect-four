5feb4fb9cac73d4683a0ae444e7d998f55536bae
jsntn12 <jsantana@nperts-mbp.mynetworksettings.com>
2023-03-30 23:56:12 -0400

    feat: Players will receive an alert when the game is a draw

--- connect4.js
+++ connect4.js
@@ -109,6 +109,9 @@ function handleClick(evt) {
 
 	// check for tie
 	// TODO: check if all cells in board are filled; if so call, call endGame
+	if (board.every((row) => row.every((cell) => cell))) {
+		return endGame('The game is a draw! \n Try again');
+	}
 
 	// switch players
 	// TODO: switch currPlayer 1 <-> 2
